206564126ef907c8b64800a5b9656a23214066e2
Joshua Tetteh <joshuatettehx@gmail.com>
2026-09-24 19:22:32 +0000

    Update LinkedIn profile link

--- lib/theme/social_links.dart
+++ lib/theme/social_links.dart
@@ -2,5 +2,5 @@ part of '../main.dart';
 
 abstract final class SocialLinks {
   static const github = 'https://github.com/jonakote';
-  static const linkedin = 'https://www.linkedin.com/in/joshua-tetteh';
+  static const linkedin = 'https://www.linkedin.com/in/joshua-nk-tetteh';
 }
